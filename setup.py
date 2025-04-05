b76bf098de422478bc279d31726b2e4d29f54920
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2025-04-05 15:05:31 +0200

    Create a package to easy install and integration

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='molgpka',
+    version='1.1',
+    author='',
+    author_email='',
+    description='A tool for pKa prediction using graph-convolutional neural network models.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ci-lab-cz/MolGpKa',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        # 'torch>=1.4',
+        # 'torch-geometric',
+        # 'rdkit',
+        # Add other dependencies from environment.yml if necessary
+    ],
+)
