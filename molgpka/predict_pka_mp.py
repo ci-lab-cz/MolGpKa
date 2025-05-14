09e3e561eb4285f614607d64c9f24f79969cd2cb
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2025-05-14 14:41:29 +0200

    Add a function predict2 to work directly with loaded models

--- molgpka/predict_pka_mp.py
+++ molgpka/predict_pka_mp.py
@@ -71,6 +71,16 @@ def predict(mol, state_dicts, uncharged=True):
     acid_dict = predict_acid(mol, models['acid'])
     return base_dict, acid_dict, mol
 
+def predict2(mol, models, uncharged=True):
+    if uncharged:
+        un = rdMolStandardize.Uncharger()
+        mol = un.uncharge(mol)
+        mol.UpdatePropertyCache()
+    mol = AllChem.AddHs(mol)
+    base_dict = predict_base(mol, models['base'])
+    acid_dict = predict_acid(mol, models['acid'])
+    return base_dict, acid_dict, mol
+
 def predict_for_protonate(mol, uncharged=True):
     if uncharged:
         un = rdMolStandardize.Uncharger()
